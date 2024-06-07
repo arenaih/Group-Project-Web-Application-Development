aea0764c314f073a205e6b5e45f6c277ae1213be
arenaih <anasredza14@gmail.com>
2024-06-08 05:53:04 +0800

    DineConnect

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -49,8 +49,8 @@
     <div class="topbar d-flex align-items-center">
       <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-          <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
-          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+          <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">dineconnect@gmail.com</a></i>
+          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+012 345 6789</span></i>
         </div>
         <div class="languages d-none d-md-flex align-items-center">
           <ul>
@@ -67,7 +67,7 @@
         <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
           <!-- Uncomment the line below if you also wish to use an image logo -->
           <!-- <img src="assets/img/logo.png" alt=""> -->
-          <h1 class="sitename">Restaurantly</h1>
+          <h1 class="sitename">DineConnect</h1>
         </a>
 
         <nav id="navmenu" class="navmenu">
@@ -182,13 +182,13 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Restaurantly</strong> <span>All Rights Reserved</span></p>
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">DineConnect</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://bootstrapmade.com/">DineConnect</a>
       </div>
     </div>
 
